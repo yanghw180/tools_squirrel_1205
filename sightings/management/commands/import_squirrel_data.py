97371528ef42168bf523081b3b07c46e9dafe4bb
Harvey <yanghw180@gmail.com>
2019-12-05 04:42:53 +0000

    basically finish load data

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -1,10 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
 from sightings.models import Squirrel
 import csv
+import datetime
 
 # reference: https://docs.djangoproject.com/en/2.2/howto/custom-management-commands/
 class Command(BaseCommand):
     help = 'A command that can be used to import the data from a given path'
+    def bool(self, b):
+        if b.lower() == "true":
+            return True
+        else:
+            return False
 
     def add_arguments(self, parser):
         parser.add_argument('path',help='/path/to/file.csv',type=str)
@@ -13,19 +19,36 @@ class Command(BaseCommand):
         path = options['path']
         # reference: https://stackoverflow.com/questions/2459979/how-to-import-csv-data-into-django-models
         with open(path) as f:
-            reader = csv.reader(f)
-            first_row = 1
+            reader = csv.DictReader(f)
+            data = list(reader)
 
-            for row in reader:
-
-                if first_row == 1:
-                    first_row =0
-                    continue
-
+            for item in data:
                 _, created = Squirrel.objects.get_or_create(
-                    longitude = float(row[0]),
-                    latitude = float(row[1]),
-                    squirrel_id = row[2],
+                    longitude = float(item['X']),
+                    latitude = float(item['Y']),
+                    squirrel_id = item['Unique Squirrel ID'],
+                    shift = item['Shift'],
+                    date = datetime.date(int(item['Date'][-4:]),int(item['Date'][:2]),int(item['Date'][2:4])),
+                    age = item['Age'],
+                    color = item['Primary Fur Color'],
+                    location = item['Location'],
+                    specific_location = item['Specific Location'],
+                    running = self.bool(item['Running']),
+
+                    chasing = self.bool(item['Chasing']),
+                    climbing = self.bool(item['Climbing']),
+                    eating = self.bool(item['Eating']),
+                    foraging = self.bool(item['Foraging']),
+                    other_activities = item['Other Activities'],
+                    kuks = self.bool(item['Kuks']),
+                
+                    quaas = self.bool(item['Quaas']),
+                    moans = self.bool(item['Moans']),
+                    tail_flags = self.bool(item['Tail flags']),
+                    tail_twitches = self.bool(item['Tail twitches']),
+                    approaches = self.bool(item['Approaches']),
+                    indifferent = self.bool(item['Indifferent']),
+                    runs_from = self.bool(item['Runs from']),
                 )
             # creates a tuple of the new object or
             # current object and a boolean of if it was created
--- sightings/models.py
+++ sightings/models.py
@@ -10,6 +10,140 @@ class Squirrel(models.Model):
     squirrel_id = models.CharField(help_text = _('Unique Squirrel ID'),
         max_length=50)
 
+
+    AM = 'AM'
+    PM = 'PM'
+
+    shift = models.CharField(
+        help_text=_('Shift'),
+        max_length=2,
+        choices =((AM,'AM'),(PM,'PM'),),
+        default=AM,
+    )
+
+    date = models.DateField('Date')
+
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    UNKNOWN = 'Unknown'
+
+    age = models.CharField(
+            help_text = _('Age'),
+            max_length = 50,
+            choices = ((ADULT,'Adult'),(JUVENILE,'Juvenile'),(UNKNOWN,'Unknown'),),
+            default = UNKNOWN,
+            )
+    
+    GRAY = 'Gray'
+    CINNAMON = 'Cinnamon'
+    BLACK = 'Black'
+
+    COLOR_CHOICES = (
+        (GRAY, 'Gray'),
+        (CINNAMON, 'Cinnamon'),
+        (BLACK, 'Black'),
+        (UNKNOWN, 'Unknown'),
+    )
+
+    color = models.CharField(
+        help_text=_('Primary Fur Color'),
+        max_length=50,
+        choices=COLOR_CHOICES,
+        default=UNKNOWN,
+    )
+
+
+    GROUND_PLANE = 'Ground Plane'
+    ABOVE_GROUND = 'Above Ground'
+
+    LOCATION_CHOICES = (
+        (GROUND_PLANE, 'Ground Plane'),
+        (ABOVE_GROUND, 'Above Ground'),
+        (UNKNOWN, 'Unknown'),
+    )
+
+    location = models.CharField(
+        help_text=_('Location'),
+        max_length=50,
+        choices=LOCATION_CHOICES,
+        default=UNKNOWN,
+    )
+
+    specific_location = models.CharField(
+        help_text=_('Specific Location'),
+        max_length=100,
+    )
+
+    running = models.BooleanField(
+        help_text = _('Running'),
+        default=False,
+    )
+
+    chasing = models.BooleanField(
+        help_text = _('Chasing'),
+        default=False,
+    )
+
+    climbing = models.BooleanField(
+        help_text = _('Climbing'),
+        default=False,
+    )
+
+    eating = models.BooleanField(
+        help_text = _('Eating'),
+        default=False,
+    )
+
+    foraging = models.BooleanField(
+        help_text = _('Foraging'),
+        default=False,
+    )
+
+    other_activities = models.CharField(
+        help_text = _('Other Activities'),
+        max_length=100,
+    )
+
+    kuks = models.BooleanField(
+        help_text = _('Kuks'),
+        default=False,
+    )
+
+    quaas = models.BooleanField(
+        help_text = _('Quaas'),
+        default=False,
+    )
+
+    moans = models.BooleanField(
+        help_text = _('Moans'),
+        default=False,
+    )
+
+    tail_flags = models.BooleanField(
+        help_text = _('Tail Flags'),
+        default=False,
+    )
+
+    tail_twitches = models.BooleanField(
+        help_text = _('Tail Twitches'),
+        default=False,
+    )
+
+    approaches = models.BooleanField(
+        help_text = _('Approaches'),
+        default=False,
+    )
+
+    indifferent = models.BooleanField(
+        help_text = _('Indifferent'),
+        default=False,
+    )
+
+    runs_from = models.BooleanField(
+        help_text = _('Runs From'),
+        default=False,
+    )
+
     def __str__(self):
         return self.squirrel_id
 
